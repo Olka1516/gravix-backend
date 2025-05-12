79f4f369e2d1f930d9f8f46b3d1e985f79693283
Olka1516 <olkakorolcuk@gmail.com>
2025-05-12 22:55:23 +0300

    GR-8: Created add songs to playlists function

--- src/routes/playlist.rotes.ts
+++ src/routes/playlist.rotes.ts
@@ -1,4 +1,5 @@
 import {
+  addSongToPlaylist,
   createPlayList,
   deletePlayList,
   getMyPlayList,
@@ -21,6 +22,7 @@ router.get("/get/:username", authMiddleware, getPublicPlaylists);
 router.get("/:id", authMiddleware, getPublicPlaylistById);
 router.delete("/:id", authMiddleware, deletePlayList);
 router.put("/update/:id", authMiddleware, updatePlayList);
+router.put("/add/song", authMiddleware, addSongToPlaylist);
 router.patch("/like/:id", authMiddleware, patchLike);
 router.patch("/dislike/:id", authMiddleware, patchDislike);
 
