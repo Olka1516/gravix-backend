17a1ddfbaf0550196504abd7da315d9662018834
Korolchuk-Olha <javascriptprogrammer@davydovconsulting.com>
2025-05-24 16:52:27 +0300

    change cors

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,24 @@ import express, { Application, json, urlencoded } from "express";
 dotenv.config();
 const port = process.env.PORT || 8000;
 const app: Application = express();
+const devMode = process.env.MODE === "dev";
 
 connectDB();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
-app.use(cors());
 app.use(fileUpload());
+
+app.use(
+  cors({
+    origin: devMode
+      ? ["http://localhost:5173", "http://localhost:4173"]
+      : "https://gravix-frontend.vercel.app",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/playlists", playlistRoutes);
