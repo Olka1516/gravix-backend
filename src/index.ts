57ffe4f74bdf58fdd9ec0b8772ddaabcb856bd29
Olka1516 <olkakorolcuk@gmail.com>
2025-05-24 17:09:12 +0300

    Change config

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.use(
     origin: devMode
       ? ["http://localhost:5173", "http://localhost:4173"]
       : "https://gravix-frontend.vercel.app",
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    methods: "GET,POST,PATCH,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
   })
